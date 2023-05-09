922dcc1f8623097241a925241a7febfc44b8d9c2
luke.nguyen <loc.thnguyen@katalon.com>
2023-05-09 14:41:14 +0700

    init

---
+++ Test Suites/simple case.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>simple case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>50</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>12a685aa-f7f9-4644-8bf3-da07c9226715</testSuiteGuid>
+   <testCaseLink>
+      <guid>a1b610f8-b4df-4508-838b-a75b3c053bf1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test-flow-1-buy-1-product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
